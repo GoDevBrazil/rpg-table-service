859fcf095fea04b1d7cf0a31b5a7c14493d98b6a
guilhermedevel <guilherme.zarzur@gmail.com>
2023-02-23 21:36:11 -0300

    feat(RPG003): Setup project using Spring Initiazr.

---
+++ src/main/java/com/godev/RpgTableServiceApplication.java
@@ -0,0 +1,13 @@
+package com.godev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgTableServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgTableServiceApplication.class, args);
+	}
+
+}
