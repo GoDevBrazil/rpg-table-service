ecb86dc67503425f626194a3f42741920dc562b4
guilhermedevel <guilherme.zarzur@gmail.com>
2023-04-05 16:03:22 -0300

    feat(RPG008): Create Account Repository

---
+++ src/main/java/com/godev/domain/repositories/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.godev.domain.repositories;
+
+import com.godev.domain.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+
+}
